c5b1b1d8a10b12199683a94db684c5792f72bfed
Hadi <hadi20107@hotmail.co.uk>
2016-08-19 18:15:44 +0100

    Delete register.php

--- lr/register.php
+++
@@ -1,4 +0,0 @@
-<?php include 'includes/overall/OVERALLheader.php' ; ?>         
-        <h1>Register</h1>
-        <p>Registration Page</p>
-<?php include 'includes/overall/OVERALLfooter.php' ; ?>
